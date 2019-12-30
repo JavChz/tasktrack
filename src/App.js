9aa9b438da0999456bae10c835270045d333e0fb
Javier <javgarchz@gmail.com>
2019-12-30 16:27:31 -0700

    added localstorage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Counter from "./components/Counter";
 
 function App() {
+  //const [current, setCurrent] = useState({});
+  let initTaks = 0;
+  if (localStorage.hasOwnProperty("tasks")) {
+    initTaks = Number(localStorage.getItem("tasks"));
+    console.log(initTaks);
+  }
+
+  const [tasks, setTasks] = useState(localStorage.getItem("tasks"));
+  const modtask = function(type) {
+    let currentTask = tasks;
+    switch (type) {
+      case "plus":
+        currentTask++;
+        break;
+      case "minus":
+        currentTask--;
+        break;
+    }
+    localStorage.setItem("tasks", currentTask);
+    setTasks(currentTask);
+  };
+  const isDisabled = function(condition) {
+    if (condition) {
+      return true;
+    }
+    return false;
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{tasks}</h1>
+      <button onClick={() => modtask("plus")}>+1</button>
+      <button
+        onClick={() => modtask("minus")}
+        disabled={isDisabled(tasks <= 0)}
+      >
+        -1
+      </button>
+      <Counter></Counter>
     </div>
   );
 }
