b6a2ed83b79824aaf7ec725075a112d8c6df8564
Javier <javgarchz@gmail.com>
2020-01-06 01:46:47 -0700

    a

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Counter from "./components/Counter";
+
+
 
 function App() {
   //const [current, setCurrent] = useState({});
   let initTaks = 0;
   if (localStorage.hasOwnProperty("tasks")) {
     initTaks = Number(localStorage.getItem("tasks"));
-    console.log(initTaks);
   }
+  console.log(initTaks);
+  
+  const [tasks, setTasks] = useState(initTaks);
+  const [timeIn, setTimeIn] = useState(0);
+  const [timeInCurrent, setTimeInCurrent] = useState(0);
+  const [lastClick, setLastClick] = useState(new Date().getTime());
+  const [archive, setArchive] = useState([]);
+  // init ClickTime
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeInCurrent(new Date().getTime() - lastClick);
+    }, 1000);
 
-  const [tasks, setTasks] = useState(localStorage.getItem("tasks"));
+    return () => {
+      clearInterval(interval);
+    };
+  }, [lastClick]);
+
   const modtask = function(type) {
-    let currentTask = tasks;
+    let currentTask = tasks;  
+
     switch (type) {
       case "plus":
+        setTimeIn(new Date().getTime() - lastClick);
+        console.log({timeIn,lastClick});
+        setLastClick(new Date);
+        // Tasks
         currentTask++;
         break;
       case "minus":
         currentTask--;
+        break;
+      case "reset":
+        currentTask = 0;
         break;
     }
     localStorage.setItem("tasks", currentTask);
@@ -30,9 +55,17 @@ function App() {
     }
     return false;
   };
+
+  const handleChange = function(event) {
+      setTasks(event.target.value);
+  }
   return (
     <div className="App">
-      <h1>{tasks}</h1>
+      <h1 >{tasks}</h1>
+      <h1>Tiempo tare actual {new Date(timeInCurrent).toISOString().substr(11, 8)}</h1>
+      <h2>last click at: {new Date(lastClick).toLocaleTimeString()}</h2>
+      <h2>Time in last task: {new Date(timeIn).toISOString().substr(11, 8) }</h2>
+      <input type="text" value={tasks} onChange={handleChange} />
       <button onClick={() => modtask("plus")}>+1</button>
       <button
         onClick={() => modtask("minus")}
@@ -40,9 +73,17 @@ function App() {
       >
         -1
       </button>
-      <Counter></Counter>
+      {archive.map(t =><SingleTask task={t}></SingleTask>)}
+      
     </div>
   );
 }
-
+function SingleTask(props){
+  return(
+    <div>
+      {props.task.id}
+      {props.task.timeIn}
+    </div>
+  )
+}
 export default App;
