b4d2c3e1b7b5fdb17d15a571cf63e787afb9899d
Javier <13250549+JavChz@users.noreply.github.com>
2021-03-23 13:38:59 -0700

    new build

---
+++ build/precache-manifest.cce5642cec8f2987dafa2d1f74265d52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e6e8b386a2784eab21e2ab5464f83d6",
+    "url": "/tasktracker/index.html"
+  },
+  {
+    "revision": "1fc02dca4d0f92a74230",
+    "url": "/tasktracker/static/css/main.4435a5c2.chunk.css"
+  },
+  {
+    "revision": "52feb825b68481a15470",
+    "url": "/tasktracker/static/js/2.0b16edf2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tasktracker/static/js/2.0b16edf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fc02dca4d0f92a74230",
+    "url": "/tasktracker/static/js/main.db4dd3f8.chunk.js"
+  },
+  {
+    "revision": "9a64d10f4d95c82e2ae6",
+    "url": "/tasktracker/static/js/runtime-main.5c6dc750.js"
+  }
+]);
